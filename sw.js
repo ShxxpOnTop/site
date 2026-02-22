8046cb4363a2a2262bcf8432cb50b6e07cd3f650
ShxxpOnTop <bobby.perfect11@gmail.com>
2026-02-22 21:14:04 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
 self.addEventListener('push', function(event) {
     const options = {
-        body: event.data ? event.data.text() : 'Nouvelle mise à jour système.',
+        body: event.data ? event.data.text() : 'Mise à jour du coffre.',
         icon: 'https://img.icons8.com/ios-filled/512/8b5cf6/flash-on.png',
-        badge: 'https://img.icons8.com/ios-filled/512/8b5cf6/flash-on.png'
+        vibrate: [100, 50, 100],
+        data: { dateOfArrival: Date.now() }
     };
+    event.waitUntil(self.registration.showNotification('Alex Vault', options));
+});
 
-    event.waitUntil(
-        self.registration.showNotification('Alex Vault', options)
-    );
+// Permet de cliquer sur la notif pour ouvrir l'appli
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(clients.openWindow('/dashboard.html'));
 });
